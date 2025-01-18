a5abb7c43d07289d76a08355e057daff446d80bf
Matik - PC <mateuszkowalski199700@gmail.com>
2025-01-18 14:43:46 +0100

    change angular version form v17 to v19

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'NextPlease' title`, () => {
+  it(`should have the 'frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NextPlease');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, NextPlease');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
 });
